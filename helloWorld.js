ad1485b467b525015c60e1a757809487239f4902
BRID19 <74292639+BRID19@users.noreply.github.com>
2026-04-16 20:06:16 +0100

    update

--- helloWorld.js
+++ helloWorld.js
@@ -1,21 +1,37 @@
 import http from "http"
+import fs from "fs"
+
+function serveStaticFile(res, path, contentType, responseCode) {
+    if(!responseCode) responseCode = 200;
+    fs.readFile(__dirname + path, function(err, data) {
+        if(err) {
+            res.writeHead(500, { 'Content-Type': 'text/plain' });
+            res.end("500 - Internal Error");
+        } else {
+            res.writeHead(responseCode,
+                { 'Content-Type': contentType });
+            res.end(data)
+        }
+    });
+}
 
 http.createServer(function(req, res){
     // normalize url by removing querystring, optional
     // trailing slash, and making it lowercase
-    let path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
+    let path = req.url.replace(/\/?(?:\?.*)?$/, '')
+                .toLowerCase();
     switch(path) {
         case '':
-            res.writeHead(200, { 'Content-Type': 'text/plain' });
-            res.end('Homepage');
+            serveStaticFile(res, '/public/home.html', 'text/html');
             break;
         case '/about':
-            res.writeHead(200, { 'Content-Type': 'text/plain'});
-            res.end('About')
+            serveStaticFile(res, '/public/about.html', 'text/html');
+            break;
+        case '/img/logo.jpg':
+            serveStaticFile(res, '/public/img/logo.jpg', 'image/jpeg');
             break;
         default:
-            res.writeHead(404, { 'content-type': 'text/plain' });
-            res.end('Not Found');
+            serveStaticFile(res, '/public/404.html', 'text/html', 404)
             break;
     }
 }).listen(3000);
